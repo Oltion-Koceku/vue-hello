03d4fd2b2c2c858e5e54e1a9db2a2f0b3aa75b04
Oltion-Koceku <koceku17@gmail.com>
2024-03-11 14:46:50 +0100

    [add]Data,ora,minuti,secondi

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-      messaggio: "Hello World!!"
+      
+      messaggio: "Hello World!!",
+      clock: "00:00:00"
+
     }
+  },
+  methods:{
+    printClock(){
+      const d = new Date();
+      const h = d.getHours() < 10 ? "0" + d.getHours() : d.getHours();
+      const m = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
+      const s = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
+      this.clock = `${h}:${m}:${s}`
+    },
+    intervalClock(){
+      setInterval(()=>{
+      this.printClock()
+      }, 1000)
+    }
+  },
+  mounted(){
+    this.printClock()
+    this.intervalClock()
   }
 }).mount("#app")
